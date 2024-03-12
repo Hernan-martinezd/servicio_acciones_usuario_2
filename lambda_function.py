857cddfcf324d68c8eb6327d7d791dddec0d19b7
Hernan <hd.martinezd1@uniandes.edu.co>
2024-03-11 22:55:48 -0500

    Updated Lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,16 @@
+import boto3
 import awsgi
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/monitor')
-def monitor():
-    return jsonify(status= 200, message= 'Echo: I\'m good')
+@app.route('/deportistas', methods=['GET'])
+def deportistas():
+    return jsonify(status= 200, message= 'Echo: GET')
+
+@app.route('/deportistas', methods=['POST'])
+def deportistas():
+    return jsonify(status= 200, message= 'Echo: POST')
 
 def lambda_handler(event, context):
     return awsgi.response(app, event, context, base64_content_types={"image/png"})
